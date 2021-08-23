cc414950d5f52c01ee4e4197a17b2529d5721b85
amit-tech01 <amit.kumar@ecerasystem.com>
2021-08-23 22:24:30 +0530

    icons added for logout

--- app/src/main/java/com/ammy/jobportalapp/MainActivity.java
+++ app/src/main/java/com/ammy/jobportalapp/MainActivity.java
@@ -36,9 +36,9 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         //logout code
-//        if(mAuth.getCurrentUser() != null){
-//
-//        }
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        }
 
         mDialog= new ProgressDialog(this);
         LoginFunction();
